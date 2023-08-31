b462751ece50422c3fc8d0637ea822392d7298fa
Rajdeep Das <rajdeep.das@hotmail.com>
2023-08-31 06:11:06 +0530

    Updated Deployer

---
+++ ConfigShell/deployer.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Update the Os
+sudo apt-get update
+sudo apt-get -y upgrade
+
+# Install Ansible
+
+sudo apt install ansible -y
+
+# Install Git
+
+sudo apt install git -y
+
+
+# Web Get the SelfhHosted agent
+
+wget https://vstsagentpackage.azureedge.net/agent/3.225.0/vsts-agent-linux-x64-3.225.0.tar.gz
+
+mkdir myagent
+cd myagent
+tar zxvf ~/vsts-agent-linux-x64-3.225.0.tar.gz

b13f571f0c3fc80fd44a4d65eb14423f9c1585b5
RAJDEEP DAS <rajdeep.das@hotmail.com>
2023-09-09 18:09:29 +0000

    Updated deployer.sh

--- ConfigShell/deployer.sh
+++ ConfigShell/deployer.sh
@@ -2,7 +2,7 @@
 
 export DEBIAN_FRONTEND=noninteractive
 export DEBIAN_PRIORITY=critical
-sudo echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
+
 # Update the Os
 sudo apt-get update
 sudo apt-get -y upgrade

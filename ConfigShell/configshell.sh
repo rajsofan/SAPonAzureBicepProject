3f2e2d9d02be3878599bf0e7d7dee50be9235aea
RAJDEEP DAS <rajdeep.das@hotmail.com>
2023-09-10 18:06:50 +0000

    Updated configshell.sh

--- ConfigShell/configshell.sh
+++ ConfigShell/configshell.sh
@@ -5,4 +5,4 @@ export DEBIAN_PRIORITY=critical
 sudo apt-get install dialog apt-utils -y
 
 
-./config.sh --unattended --url https://dev.azure.com/AzDevOpsIAC --auth pat --token myToken --pool default --agent myAgent --acceptTeeEula
+./config.sh --unattended --url https://dev.azure.com/AzDevOpsIAC --auth pat --token 4os3qekbpt5gzeb26lqqwyvorh5qvdzo25olxi76x2fnbntdpfhq --pool default --agent myAgent --acceptTeeEula
